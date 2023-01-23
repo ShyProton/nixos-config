ebaada783d44d097f80c19a0c741f600d7b87474
SaiProton <shayanr1001@gmail.com>
2023-01-23 17:27:40 -0500

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    home-manager.url = github:nix-community/home-manager;
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      lib = nixpkgs.lib;
+    in
+    {
+      homeConfigurations = {
+        "shayanr@vbox" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home/vbox.nix
+          ];
+        };
+      };
+
+      nixosConfigurations = {
+        vbox = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./hosts/vbox
+          ];
+        };
+      };
+    };
+}
