3b2d05c265503b90bd88ccccf62eb9a9cae261ce
SaiProton <shayanr1001@gmail.com>
2023-02-03 22:06:30 -0500

    Preferred options added to neovim

--- home/shayanr/features/nvim/default.nix
+++ home/shayanr/features/nvim/default.nix
@@ -10,5 +10,52 @@
     ./ui
   ];
 
-  programs.nixvim.enable = true;
+  programs.nixvim = {
+    enable = true;
+
+    options = {
+      number = true;
+      relativenumber = true;
+      numberwidth = 4;
+      signcolumn = "yes";
+      colorcolumn = "80";
+      synmaxcol = 1024;
+      cursorline = true;
+
+      scrolloff = 8;
+      sidescrolloff = 8;
+      wrap = false;
+
+      smartindent = true;
+      autoindent = true;
+
+      splitbelow = true;
+      splitright = true;
+
+      shiftwidth = 4;
+      tabstop = 8;
+      softtabstop = 4;
+      showtabline = 4;
+      expandtab = true;
+
+      hlsearch = true;
+      ignorecase = true;
+      smartcase = true;
+
+      timeout = true;
+      timeoutlen = 500;
+      updatetime = 300;
+      ttimeoutlen = 10;
+
+      backup = false;
+      swapfile = false;
+
+      clipboard = "unnamedplus";
+      mouse = "a";
+
+      showmode = false;
+      pumheight = 10;
+      termguicolors = true;
+    };
+  };
 }
