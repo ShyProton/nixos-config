b9f476927e104781a50ae3a02ef014aaa1d03439
SaiProton <shayanr1001@gmail.com>
2023-02-06 09:01:07 -0500

    Catppuccin theme updated to fit preferences, lspsaga added

--- home/shayanr/features/nvim/langs/lsp.nix
+++ home/shayanr/features/nvim/langs/lsp.nix
@@ -3,7 +3,45 @@
     lsp = {
       enable = true;
 
-      servers = { };
+      servers = {
+        rnix-lsp.enable = true;
+      };
+    };
+
+    lspsaga = {
+      enable = true;
+
+      headers = {
+        hint = " ";
+        info = " ";
+        warning = " ";
+        error = " ";
+      };
+
+      signs = {
+        hint = " ";
+        info = " ";
+        warning = " ";
+        error = " ";
+      };
+
+      keys = {
+        finderAction = {
+          quit = "q";
+          vsplit = "v";
+
+          scrollDown = "<Tab>";
+          scrollUp = "<S-Tab>";
+        };
+
+        codeAction = {
+          quit = "q";
+        };
+      };
+
+      icons = {
+        codeAction = "";
+      };
     };
   };
 }
