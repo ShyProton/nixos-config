363311a1095e92af270fb64912aea0364c67c2fd
SaiProton <shayanr1001@gmail.com>
2023-02-07 18:54:08 -0500

    Added host ssh keys to openssh configuration

--- hosts/vbox/default.nix
+++ hosts/vbox/default.nix
@@ -35,10 +35,16 @@
 
   services.openssh = {
     enable = true;
-    hostKeys = [{
-      path = "/persist/etc/ssh/ssh_host_ed25519_key";
-      type = "ed25519";
-    }];
+    hostKeys = [
+      {
+        path = "/persist/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        path = "/persist/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+      }
+    ];
   };
 
   # TODO: Probably do this using overlays
