01c4a26e1d215a08fb71ea243bc08aac123c4151
SaiProton <shayanr1001@gmail.com>
2023-03-29 01:21:08 -0400

    feat: begins to add templates for projects to the flake

---
+++ templates/default.nix
@@ -0,0 +1,15 @@
+{
+  rust = {
+    path = ./rust;
+    description = "Rust environment (Cargo)";
+    welcomeText = ''
+      # Rust/Cargo Template
+      ## Intended usage
+      For getting a rust/cargo project up and running.
+
+      ## More info
+      - [Rust language](https://www.rust-lang.org)
+      - [Rust on the NixOS Wiki](https://nixos.wiki/wiki/rust)
+    '';
+  };
+}
