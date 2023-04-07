dd50ac27c6638f8ce4ca7da29eb67e7e02c58486
SaiProton <shayanr1001@gmail.com>
2023-04-06 21:05:57 -0400

    fix: temporary hack to fix broken shell prompt for new nushell version

--- home/shayanr/features/cli/shell/nushell/default.nix
+++ home/shayanr/features/cli/shell/nushell/default.nix
@@ -34,6 +34,31 @@ lib.mkIf (osConfig.users.users.shayanr.shell == pkgs.nushell) {
       }
     '';
 
+  # HACK: Waiting for https://github.com/starship/starship/issues/5063
+  xdg.configFile."nushell/env.nu".text = ''
+    let-env PROMPT_INDICATOR = {|| "❯ " }
+    let-env PROMPT_INDICATOR_VI_INSERT = {|| "❯ " }
+
+    let-env PROMPT_INDICATOR_VI_NORMAL = {|| ": " }
+    let-env PROMPT_MULTILINE_INDICATOR = {|| "::: " }
+
+    let-env ENV_CONVERSIONS = {
+      "PATH": {
+        from_string: { |s| $s | split row (char esep) | path expand -n }
+        to_string: { |v| $v | path expand -n | str join (char esep) }
+      }
+      "Path": {
+        from_string: { |s| $s | split row (char esep) | path expand -n }
+        to_string: { |v| $v | path expand -n | str join (char esep) }
+      }
+    }
+
+    starship init nu
+    | str replace --string 'PROMPT_COMMAND = {' 'PROMPT_COMMAND = { ||'
+    | str replace --string 'PROMPT_COMMAND_RIGHT = {' 'PROMPT_COMMAND_RIGHT = { ||'
+    | save -f ~/.cache/starship/init.nu
+  '';
+
   # Persists shell history between reboots.
   home.persistence."/persist${config.home.homeDirectory}".files = [
     ".config/nushell/history.txt"
