767630d5793c383472e51ff5a2bf659226e4a8d8
ShyProton <shayanr1001@gmail.com>
2023-07-15 17:42:22 -0400

    feat: adds fprintd for fingerprint verification

--- hosts/mob/default.nix
+++ hosts/mob/default.nix
@@ -26,6 +26,13 @@
   services = {
     blueman.enable = true;
     tlp.enable = true;
+    fprintd = {
+      enable = true;
+      tod = {
+        enable = true;
+        driver = pkgs.libfprint-2-tod1-goodix;
+      };
+    };
   };
 
   hardware.opengl = {
