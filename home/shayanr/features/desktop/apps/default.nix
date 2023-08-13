7d75344581dc0a400b1ba6859b69ee2902d1298f
ShyProton <shayanr1001@gmail.com>
2023-08-13 00:36:04 -0400

    fix: removes webcord due to unreliable call connectivity

--- home/shayanr/features/desktop/apps/default.nix
+++ home/shayanr/features/desktop/apps/default.nix
@@ -3,13 +3,10 @@
   # added and a config directory persisted.
   imports = [
     ./firefox # Web browser.
-    # ./discord # Discord app.
 
     ./kitty.nix # Terminal emulator.
     ./spotify.nix # Music player.
-    ./webcord.nix # Discord client.
     ./obsidian.nix # Note taking.
-    # ./idea.nix # IntelliJ IDEA.
   ];
 
   home.packages = with pkgs; [
