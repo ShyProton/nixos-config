e90b67795633a6edc248b371996bd0865d1f3bac
ShyProton <shayanr1001@gmail.com>
2023-08-16 17:45:50 -0400

    chore: bumps lock

--- home/shayanr/features/cli/nvim/langs/lsp.nix
+++ home/shayanr/features/cli/nvim/langs/lsp.nix
@@ -26,41 +26,7 @@
         };
       };
 
-      lspsaga = {
-        enable = false;
-
-        headers = {
-          hint = " ";
-          info = " ";
-          warning = " ";
-          error = " ";
-        };
-
-        signs = {
-          hint = " ";
-          info = " ";
-          warning = " ";
-          error = " ";
-        };
-
-        keys = {
-          finderAction = {
-            quit = "q";
-            vsplit = "v";
-
-            scrollDown = "<Tab>";
-            scrollUp = "<S-Tab>";
-          };
-
-          codeAction = {
-            quit = "q";
-          };
-        };
-
-        icons = {
-          codeAction = "";
-        };
-      };
+      lspsaga.enable = false;
 
       trouble = {
         enable = true;
