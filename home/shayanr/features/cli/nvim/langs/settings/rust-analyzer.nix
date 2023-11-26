803f4bb056955ac27082889bdbee5e8bbf38da3f
ShyProton <shayanr1001@gmail.com>
2023-11-26 03:36:34 -0500

    fix(nvim): specifies to not install rust-analyzer dependencies

--- home/shayanr/features/cli/nvim/langs/settings/rust-analyzer.nix
+++ home/shayanr/features/cli/nvim/langs/settings/rust-analyzer.nix
@@ -1,16 +1,19 @@
 {
-  programs.nixvim.plugins.lsp.servers
-  .rust-analyzer.settings.check.overrideCommand = [
-    "cargo"
-    "clippy"
-    "--workspace"
-    "--message-format=json"
-    "--all-targets"
-    "--all-features"
-    "--"
-    "-Wclippy::pedantic"
-    "-Wclippy::nursery"
-    "-Wclippy::unwrap_used"
-    "-Wclippy::expect_used"
-  ];
+  programs.nixvim.plugins.lsp.servers.rust-analyzer = {
+    installCargo = false;
+    installRustc = false;
+    settings.check.overrideCommand = [
+      "cargo"
+      "clippy"
+      "--workspace"
+      "--message-format=json"
+      "--all-targets"
+      "--all-features"
+      "--"
+      "-Wclippy::pedantic"
+      "-Wclippy::nursery"
+      "-Wclippy::unwrap_used"
+      "-Wclippy::expect_used"
+    ];
+  };
 }
