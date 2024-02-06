9c9298f5cabc880835d54d3c6d869228f4c0276a
ShyProton <shayanr1001@gmail.com>
2024-02-06 08:57:42 -0500

    fix(tldr): removes broken option

--- home/shayanr/features/cli/tldr.nix
+++ home/shayanr/features/cli/tldr.nix
@@ -2,6 +2,6 @@
   programs.tealdeer = {
     enable = true;
     # Stores pages in .cache automatically
-    settings.updates.auto_update = true;
+    # settings.updates.auto_update = true;
   };
 }
