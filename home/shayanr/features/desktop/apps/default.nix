6403a63fc807a1379db4b70482dab80d199c4fac
ShyProton <shayanr1001@gmail.com>
2024-12-06 14:36:48 -0500

    chore: removes unecessary packages

--- home/shayanr/features/desktop/apps/default.nix
+++ home/shayanr/features/desktop/apps/default.nix
@@ -6,18 +6,15 @@
   # TODO: Make shorthand function to quickly add apps that only need the package
   # added and a config directory persisted.
   imports = [
-    ./firefox # Web browser.
     ./zen.nix # Web browser.
 
     ./kitty.nix # Terminal emulator.
     ./spotify.nix # Music player.
     ./obsidian.nix # Note taking.
-    ./reaper.nix # Audio editing.
   ];
 
   home.packages = with pkgs; [
     pavucontrol
-    inkscape
     chromium
     nautilus
     inputs.zen-browser.packages."${pkgs.system}".default
